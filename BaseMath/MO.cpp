5712ef38f66f7fd63afea7da5b3b8ce34efe3238
zz1324584631 <1324584631@qq.com>
2025-06-02 16:23:24 +0800

    增加高斯消元求解向量函数

--- BaseMath/MO.cpp
+++ BaseMath/MO.cpp
@@ -1,6 +1,7 @@
 //矩阵运算函数
 #include<PCH.h>
 #include<Matrix.h>
+const double EPS = 1e-10; // 浮点数精度阈值
 void Matrix::InitializeMatrix() {//初始化矩阵大小
 	p = new double* [rows];//分配rows个指针
 	for (int i = 0; i < rows; ++i) {
@@ -149,6 +150,65 @@ Matrix& Matrix::operator*=(const Matrix &m)
 	}
 	return *this;
 
+}
+Matrix Matrix::Solve(const Matrix& A, const Matrix& b) {
+	// 创建增广矩阵
+	Matrix aug(A.rows, A.cols + 1);
+	for (int i = 0; i < A.rows; ++i) {
+		for (int j = 0; j < A.cols; ++j) {
+			aug.p[i][j] = A.p[i][j];
+		}
+		aug.p[i][A.cols] = b.p[i][0];
+	}
+
+	// 列主元消去法
+	for (int i = 0; i < aug.rows; ++i) {
+		// 找到当前列中绝对值最大的元素所在的行
+		int max_row = i;
+		for (int j = i + 1; j < aug.rows; ++j) {
+			if (abs(aug.p[j][i]) > abs(aug.p[max_row][i])) {
+				max_row = j;
+			}
+		}
+
+		// 交换当前行和最大行
+		if (max_row != i) {
+			for (int j = i; j < aug.cols; ++j) {
+				std::swap(aug.p[i][j], aug.p[max_row][j]);
+			}
+		}
+
+		// 消去下方元素
+		for (int j = i + 1; j < aug.rows; ++j) {
+			double factor = aug.p[j][i] / aug.p[i][i];
+			for (int k = i + 1; k < aug.cols; ++k) {
+				aug.p[j][k] -= factor * aug.p[i][k];
+				if (abs(aug.p[j][k]) < EPS) {
+					aug.p[j][k] = 0;
+				}
+			}
+			aug.p[j][i] = 0;
+		}
+	}
+
+	// 反向代换求解向量
+	Matrix x(aug.rows, 1);
+	x.p[x.rows - 1][0] = aug.p[x.rows - 1][aug.cols - 1] / aug.p[x.rows - 1][x.rows - 1];
+	if (abs(x.p[x.rows - 1][0]) < EPS) {
+		x.p[x.rows - 1][0] = 0;
+	}
+	for (int i = x.rows - 2; i >= 0; --i) {
+		double sum = 0;
+		for (int j = i + 1; j < x.rows; ++j) {
+			sum += aug.p[i][j] * x.p[j][0];
+		}
+		x.p[i][0] = (aug.p[i][aug.cols - 1] - sum) / aug.p[i][i];
+		if (abs(x.p[i][0]) < EPS) {
+			x.p[i][0] = 0;
+		}
+	}
+
+	return x;
 }
 //矩阵除
 //Matrix& Matrix::operator/=(const Matrix& m)
